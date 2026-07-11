3529761423d1852e5865826a20048d2932433ddc
mulqy al-khautsar <mulkoi92@gmail.com>
2026-07-12 01:30:58 +0700

    switch db ke mysql

--- src/main/java/com/mycompany/gudangkopi/config/Koneksi.java
+++ src/main/java/com/mycompany/gudangkopi/config/Koneksi.java
@@ -18,19 +18,17 @@ public class Koneksi {
     private static Connection konek;
     
     public static Connection getKoneksi() {
-        if(konek ==null){
+          if(konek ==null){
             try {
+                //kita menggunakan mysql 8 dan konfigurasi database lewat .env agar ter enkripsi 
                 Dotenv dotenv = Dotenv.load();
                 String url = dotenv.get("DB_URL");
                 String user = dotenv.get("DB_USER");
                 String password = dotenv.get("DB_PASSWORD");
 
-                Class.forName("org.postgresql.Driver");
                 konek = DriverManager.getConnection(url, user, password);
                 System.out.println("Koneksi Database Berhasil!");
-            } catch (ClassNotFoundException e) {
-                System.out.println("Driver PostgreSQL Tidak Ditemukan: " + e.getMessage());
-            } catch (SQLException e) {
+            }catch(SQLException e) {
                 System.out.println("Koneksi Database Gagal: " + e.getMessage());
             }
         }
